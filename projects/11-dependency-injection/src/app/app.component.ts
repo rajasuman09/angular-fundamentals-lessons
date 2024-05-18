770017b1a4f4b7103b7896abda4f4ce7fe846c6a
Raja Suman <rajasuman09@gmail.com>
2024-05-18 12:56:03 +0530

    completed 9,10,11

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: ` <h1>User Listing</h1> 
+  @for (item of userData; track $index) {
+    <app-user-info [user]="item"/>
+  }
+  `,
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.userService.getUserData().then(data => {
+      this.userData = data;
+    })
+  }
+  userService = inject(UserService);
+  userData: User[] = [];
 }
